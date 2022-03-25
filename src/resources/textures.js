7ac88e15acd99e2fa4637bb664d269c28f42855f
baikun <2749245563@qq.com>
2022-03-25 21:42:12 +0800

    my world

---
+++ src/resources/textures.js
@@ -0,0 +1,71 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-15 09:58:32
+ * @LastEditTime: 2022-03-16 09:15:00
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \AirHua-byte.github.io\src\resources\textures.js
+ */
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = '../src/jsm/news.png';
+// 第二块展板图片地址
+billboardTextures.musicTexture =
+  '../src/jsm/gl_shop.png';
+// 第三块展板图片地址
+billboardTextures.fundTexture =
+  '../src/jsm/wmyh.png';
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.BiliBili = '../src/jsm/BiliBili.png';
+boxTexture.QQ = '../src/jsm/qq.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.png';
+// 展板木头材质
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/新闻.png';
+inputText.activities = '../src/jsm/activities_text.png';
+inputText.bagholderBetsText = '../src/jsm/电商.png';
+inputText.homeSweetHomeText = '../src/jsm/外卖.png';
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+inputText.pcControl = '../src/jsm/pc-control.png'
+inputText.mobileControl = '../src/jsm/mobile-control.png'
+inputText.link = '../src/jsm/link.png'
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog =
+  'http://47.97.208.94:8080/';
+URL.ryanfloyd = 'http://47.97.208.94:8084/home';
+URL.fund = 'http://47.97.208.94:8082/';
+URL.gitHub = 'https://github.com/baikun21320';
+URL.BiliBili = 'https://www.bilibili.com/';
+URL.email = '#';
+URL.music = 'http://47.97.208.94:8084/home';
+URL.devTo =
+  '#';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
